e5e169fa8ba35735d6ccbe1038a678b7f42e44f0
Märt Bakhoff <mart.bakhoff@gmail.com>
2013-11-05 20:51:00 +0200

    human-assisted invoicing

--- src/main/java/esi/buildit9/service/InvoiceHumanAssistedHandling.java
+++ src/main/java/esi/buildit9/service/InvoiceHumanAssistedHandling.java
@@ -1,5 +1,7 @@
 package esi.buildit9.service;
 
+import esi.buildit9.domain.InvoiceStatus;
+import esi.buildit9.domain.PurchaseOrder;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.stereotype.Component;
 
@@ -8,7 +10,13 @@ public class InvoiceHumanAssistedHandling {
 
     @ServiceActivator
     public void process(InvoiceSDO invoiceSDO) {
-        throw new UnsupportedOperationException();
+        InvoiceResource invoiceResource = InvoiceHelper.unmarshall(invoiceSDO.document);
+        PurchaseOrder order = PurchaseOrder.findPurchaseOrder(invoiceResource.getPo());
+        if (Math.abs(order.getTotalPrice() - invoiceResource.getTotal()) < 0.1) {
+            InvoiceHelper.storeInvoice(invoiceResource, InvoiceStatus.PENDING);
+        } else {
+            // TODO: send email
+        }
     }
 
 }
