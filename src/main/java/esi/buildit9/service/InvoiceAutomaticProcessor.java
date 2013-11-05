ca90fb181b568e0497f9c90aca13c74675f42cb7
Brian <brian@ubuntu.(none)>
2013-11-05 21:33:30 +0200

    automatic invoice different messages

--- src/main/java/esi/buildit9/service/InvoiceAutomaticProcessor.java
+++ src/main/java/esi/buildit9/service/InvoiceAutomaticProcessor.java
@@ -26,30 +26,26 @@ public class InvoiceAutomaticProcessor {
     	float documentTotal = invoiceResource.getTotal();
     	long documentPO = invoiceResource.getPo();
     	float POTotal = PurchaseOrder.findPurchaseOrder(documentPO).getTotalPrice();
-
+    	String address = ((InternetAddress) invoiceSDO.from[0]).getAddress();
     	// Check if such PO exists
     	if (PurchaseOrder.findPurchaseOrder(documentPO) != null) {
     		// Check if Totals match
     		if (documentTotal == POTotal) {
     			// TODO
-    			JavaMailSenderImpl sender = new JavaMailSenderImpl();
-    			sender.setHost("mail.host.com");
-
-    			MimeMessage message = sender.createMimeMessage();
-    			MimeMessageHelper helper = new MimeMessageHelper(message);
-    			try {
-					helper.setTo(((InternetAddress) invoiceSDO.from[0]).getAddress());
-	    			helper.setText("Thank you for the invoice with PO" + documentPO + "!");
-				} catch (MessagingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-    			sender.send(message);
+    			sendEmail("Thank you for the invoice with PO" + documentPO + "!", address);
+			}else {
+				sendEmail("Error with invoice - totals don't match!", address);
 			}
+		} else {
+			sendEmail("Error with invoice - no such PO id!", address);
 		}
-    	
-    	
-        throw new UnsupportedOperationException();
+    }
+    
+    public MailMessage sendEmail(String text, String to){
+    	MailMessage message = new SimpleMailMessage();
+    	message.setTo(to);
+    	message.setSubject("Builtit9 invoice reply");
+    	message.setText(text);
+    	return message;
     }
 }
